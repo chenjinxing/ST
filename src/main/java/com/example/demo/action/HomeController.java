2d7d1b41eeac04a4b1e1bd950b03ecd4e8e24713
chen <1229501879@qq.com>
2018-06-28 08:27:02 +0800

    实现登录

--- src/main/java/com/example/demo/action/HomeController.java
+++ src/main/java/com/example/demo/action/HomeController.java
@@ -2,9 +2,13 @@ package com.example.demo.action;
 
 
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +30,36 @@ public class HomeController extends BaseController{
 		return "/top";
 	}
 	
+//	@RequestMapping("/login")
+//	public String login(HttpServletRequest request) {
+//		return "/login";
+//	}
 	@RequestMapping("/login")
-	public String login(HttpServletRequest request) {
-		return "/login";
+	public String login(HttpServletRequest request) throws Exception{
+	    System.out.println("HomeController.login()");
+	    // 登录失败从request中获取shiro处理的异常信息。
+	    // shiroLoginFailure:就是shiro异常类的全类名.
+	    String exception = (String) request.getAttribute("shiroLoginFailure");
+	    System.out.println("exception=" + exception);
+	    String msg = "";
+	    if (exception != null) {
+	        if (UnknownAccountException.class.getName().equals(exception)) {
+	            System.out.println("UnknownAccountException -- > 账号不存在：");
+	            msg = "UnknownAccountException -- > 账号不存在：";
+	        } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
+	            System.out.println("IncorrectCredentialsException -- > 密码不正确：");
+	            msg = "IncorrectCredentialsException -- > 密码不正确：";
+	        } else if ("kaptchaValidateFailed".equals(exception)) {
+	            System.out.println("kaptchaValidateFailed -- > 验证码错误");
+	            msg = "kaptchaValidateFailed -- > 验证码错误";
+	        } else {
+	            msg = "else >> "+exception;
+	            System.out.println("else -- >" + exception);
+	        }
+	    }
+	    request.getSession().setAttribute("msg", msg);
+	    // 此方法不处理登录成功,由shiro进行处理 return "{\"result\":false,\"code\":0,\"msg\":\"登陆失败\",\"data\":null}";
+	    return "/login";
 	}
 	
 	@RequestMapping("/main")
---
+++ src/main/java/com/example/demo/shrio/ShiroConfig.java
@@ -0,0 +1,78 @@
+package com.example.demo.shrio;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
+
+import org.apache.shiro.mgt.SecurityManager;
+@Configuration
+public class ShiroConfig {
+
+	@Bean
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+        System.out.println("ShiroConfiguration.shirFilter()");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //拦截器.
+        Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
+        // 配置不会被拦截的链接 顺序判断
+        //filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/css/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
+        filterChainDefinitionMap.put("/images/**", "anon");
+        
+        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("/logout", "logout");
+        //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
+        //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/**", "authc");
+        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl("/home/login");
+        // 登录成功后要跳转的链接
+        shiroFilterFactoryBean.setSuccessUrl("/home/main");
+
+        
+        //未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+      //其他拦截器
+//        Map<String, Filter> filters =new LinkedHashMap<String,Filter>();
+//        filters.put("authc", loginFilter());
+//        shiroFilterFactoryBean.setFilters(filters);
+        return shiroFilterFactoryBean;
+    }
+
+//	@Bean
+//    public Filter loginFilter(){
+//		CaptchaFormAuthenticationFilter myLoginFilter = new CaptchaFormAuthenticationFilter();
+//        return myLoginFilter;
+//    }
+//	@Bean  
+//	public FilterRegistrationBean registrationBean(CaptchaFormAuthenticationFilter customFormAuthenticationFilter){  
+//		System.out.println("AppcustomFormAuthenticationFilter()");  
+//		FilterRegistrationBean registration = new FilterRegistrationBean(customFormAuthenticationFilter);  
+//		registration.setEnabled(false);//怎么取消  Filter自动注册,不会添加到FilterChain中.  
+//		return registration;  
+//	}  
+
+    @Bean
+    public MyRealm myShiroRealm(){
+    	MyRealm myShiroRealm = new MyRealm();
+        return myShiroRealm;
+    }
+
+
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
+        securityManager.setRealm(myShiroRealm());
+        return securityManager;
+    }
+}
